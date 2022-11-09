e30ff1edb3166e20ab8d6ea6dc7f32c12e87acea
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2022-11-09 19:22:55 +0530

    swithc case

---
+++ 09_Nov_2022/SwitchCase.js
@@ -0,0 +1,16 @@
+var key='value';
+switch(key)
+{
+    case "value":
+        console.log('Matched');
+        break;
+    case 'other':
+        console.log("not matched");
+        break;
+
+    default:
+        console.log("default")    
+        break;
+    
+        
+}
---
+++ 09_Nov_2022/index.js
@@ -0,0 +1,39 @@
+console.log(34);
+
+var num1=2;
+var num2=10;
+
+function multiply(num1 , num2)
+{
+    const a=100;
+    var ans=num1*num2;
+    return ans;
+    console.log(a)
+}
+const c=300
+{
+    var name='Rohitash'
+    const b=200
+    console.log(b)
+}
+
+
+console.log(c)
+console.log(name);
+var result1=multiply(num1, num2);
+var result2=multiply(30,3);
+console.log(result1);
+console.log(result2);
+
+//console.log(ans);
+
+let x
+var y
+const z=1
+y=2
+x=3
+console.log(x)
+console.log(y)
+console.log(z)
+
+
