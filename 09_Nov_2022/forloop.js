d8a560422f0642588575b6e0fa24b0148392b09b
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2022-11-09 22:50:54 +0530

    for, while, do-while loop in js

---
+++ 09_Nov_2022/forloop.js
@@ -0,0 +1,22 @@
+
+
+for(var i=0;i<5;i++)
+{
+  console.log('i inside',i);
+}
+
+console.log('i outside',i)
+var i=0;
+while(i<5)
+{
+    
+    console.log(++i);
+}
+var k=0
+do{
+    console.log('k value',k)
+    k++
+}while(k<-9)
+{
+    console.log('k value in while ', k)
+}
