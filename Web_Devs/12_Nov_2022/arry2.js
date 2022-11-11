b9ee54c2b9a41321e669eaf85117036be971d8d7
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2022-11-12 03:19:39 +0530

    array manuplation

--- Web_Devs/12_Nov_2022/arry2.js
+++ Web_Devs/12_Nov_2022/arry2.js
@@ -9,18 +9,18 @@ console.log(typeof(arr_str))
 console.log(typeof(arr_str.join()))
 
 var arr1=[1,2,4,5,70,7,7,8]
-// console.log(arr1.slice())
-// console.log(arr1.slice(2))
-// console.log(arr1.slice(2,4))
+console.log(arr1.slice())
+console.log(arr1.slice(2))
+console.log(arr1.slice(2,4))
 
-// console.log(arr1.slice(-5))
+console.log(arr1.slice(-5))
 
 
-// console.log(arr1.splice(7,9))
-// arr1.push(1000)
-// arr1.push({name:'rohit',address:{city: 'dehli',pincode:1234} }),
-// console.log(arr1[1].name)
-// console.table(arr1)
+console.log(arr1.splice(7,9))
+arr1.push(1000)
+arr1.push({name:'rohit',address:{city: 'dehli',pincode:1234} }),
+console.log(arr1[1].name)
+console.table(arr1)
 
 arr1.pop()
 console.log(arr1)
